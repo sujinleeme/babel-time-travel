d13dd46fe078b21161130395b122872b3be036d8
sujinleeme <sujinlee.me@gmail.com>
2018-07-17 18:56:25 +0900

    Change var to es6 let and const

--- src/store/buffer-utils.js
+++ src/store/buffer-utils.js
@@ -4,9 +4,9 @@ export function ab2str(buf) {
 }
 
 export function str2ab(str) {
-  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
+  let buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+  let bufView = new Uint16Array(buf);
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
     bufView[i] = str.charCodeAt(i);
   }
   return buf;
